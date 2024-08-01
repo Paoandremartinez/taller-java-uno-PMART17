cd0f038e3b677c0459a8c9a3a1b8c4c0299c59ff
paoandremartinez <paoandremartinez@gmail.com>
2024-08-01 17:34:08 -0500

    Programa de lectura de nota primer taller

---
+++ ingreseNota.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println( "Ingresa la nota");
+        int nota = scanner.nextInt();
+                switch (nota)
+                {
+                    case 1:
+                        System.out.println("Su calificación es F ");
+                        break;
+                    case 2:
+                        System.out.println("Su calificación es F ");
+                        break;
+                    case 3:
+                    case 4:
+                        System.out.println("Su calificación es D");
+                        break;
+                    case 5:
+                    case 6:
+                        System.out.println("Su calificación es C");
+                        break;
+                    case 7:
+                    case 8:
+                        System.out.println("Su calificación es B");
+                        break;
+                    case 9:
+                    case 10:
+                        System.out.println("Su calificación es A");
+                        break;
+                    default:
+                        System.out.println("Número no válido, por favor verifique");
+
+                    }
+                }
+		}
